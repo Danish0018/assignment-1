2a685d43958350b9bf5ec78371442838d1aaa094
Your Name <you@example.com>
2024-09-18 01:03:10 +0530

    NaturalNumber

---
+++ question9.c
@@ -0,0 +1,14 @@
+/*•	Problem: Write a C program that uses a for loop to print the first 10 natural numbers (1 to 10).*/
+#include<stdio.h>
+void main ()
+{
+    int number;
+    printf("Enter Value : ");
+    scanf("%d",&number);
+
+    for(int i = 1; i <= number; i ++){
+        printf("%d ", i);
+    }
+
+
+}
