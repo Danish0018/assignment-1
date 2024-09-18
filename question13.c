55e2362ec07842719e5b1574e0e78615c7a50ad1
Your Name <you@example.com>
2024-09-18 09:26:59 +0530

    reverse

---
+++ question13.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+void main()
+{
+    int number, reverse;
+
+    printf("Enter Value : ");
+    scanf("%d", &number);
+    printf("The reverse is : ");
+
+    while (number > 0)
+    {
+        reverse = number % 10;
+        printf("%d", reverse);
+        number = number / 10;
+    }
+     
+}
